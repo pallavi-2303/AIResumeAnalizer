5feb0dfb5217ffd1175ebc6461ff8ef5694cf209
pallavi-2303 <singh.pallavikumari14@gmail.com>
2025-07-21 02:54:32 +0530

    Fetch real resumes on the home page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,25 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router'
-import ScoreCircle from './ScrollCircle'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import ScoreCircle from "./ScrollCircle";
+import { usePuterStore } from "~/lib/puter";
 
-const ResumeCard = ({resume:{id,companyName,jobTitle,feedback,resumePath,imagePath}}:{resume:Resume}) => {
+const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback,  imagePath },
+}: {
+  resume: Resume;
+}) => {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [imagePath]);
   return (
-  <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
-    <div className='resume-card-header'>
-    <div className='flex flex-col gap-2'>
-<h2 className='!text-black font-bold break-words'>{companyName}</h2>
-<h3 className='text-lg brek-words text-gray-500'>{jobTitle}</h3>
-    </div>
-    <div className='flex-shrink-0'>
-<ScoreCircle score={feedback.overallScore}/>
-    </div></div>
-    <div className='gradient-border animate-in fade-in duration-1000'>
-      <div className='h-full w-full'>
-<img src={imagePath} alt="resume" className='w-full h-[350px] object-cover object-top '/>
+    <Link
+      to={`/resume/${id}`}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          {companyName && (
+            <h2 className="!text-black font-bold break-words">{companyName}</h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="!text-black font-bold">Resume</h2>
+          )}
+        </div>
+        <div className="flex-shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
       </div>
-    </div>
-  </Link>
-  )
-}
+      {resumeUrl && (
+   <div className="gradient-border animate-in fade-in duration-1000">
+        <div className="h-full w-full">
+          <img
+            src={resumeUrl}
+            alt="resume"
+            className="w-full h-[350px] object-cover object-top "
+          />
+        </div>
+      </div>
+      )}
+    </Link>
+  );
+};
 
-export default ResumeCard
+export default ResumeCard;
